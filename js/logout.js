06bc4baa9a2e0ccf280df0f06f40de663726d2f3
recinosvictor2004 <recinosvictor2004@gmail.com>
2026-06-25 22:47:02 -0600

    feat: :bug: arregle un bug que hice mal en poner login

--- js/logout.js
+++ js/logout.js
@@ -7,7 +7,7 @@ if (logoutBtn) {
     logoutBtn.addEventListener("click", async () => {
         try {
             await signOut(auth);
-            window.location.href = "login.html";
+            window.location.href = "index.html";
         } catch (error) {
             console.error("Error al cerrar sesión:", error);
             alert("No se pudo cerrar sesión");
